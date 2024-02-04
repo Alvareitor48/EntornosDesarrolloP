e6e12f984b4e0253de0912da6c798c4b594831e7
Alvaro Mayol <156838776+Alvareitor48@users.noreply.github.com>
2024-02-04 20:17:12 +0100

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -1,5 +1,16 @@
+import java.util.*;
 public class Prueba{
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        try {
+			while(true) {
+				System.out.println(fecha());
+				Thread.sleep(1000);
+			}
+		}catch (Exception e) {
+		}
+    }
+    public static String fecha(){
+        Calendar date = Calendar.getInstance();
+		return date.get(Calendar.DAY_OF_MONTH)+"/"+(date.get(Calendar.MONTH)+1)+"/"+date.get(Calendar.YEAR)+"  "+date.get(Calendar.HOUR)+"/"+(date.get(Calendar.MINUTE)+1)+"/"+date.get(Calendar.SECOND);
     }
 }
