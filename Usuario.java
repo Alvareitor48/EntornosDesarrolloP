ce57183b4cee1744ed3e630962804b9bab19081d
Alvaro Mayol <156838776+Alvareitor48@users.noreply.github.com>
2024-02-04 20:38:33 +0100

    Usuario.java

--- Usuario.java
+++ Usuario.java
@@ -32,6 +32,8 @@ public class Usuario{
 		public void setEmail(String email) {
 			this.email = email;
 		}
-	  
+	  	public String toString() {
+			return "Usuario [nombre=" + nombre + ", apellidos=" + apellidos + ", email=" + email + "]";
+		}
   
 }
